3516ea40399004b89238b9f5c0d05fdf2567bb47
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-04-14 15:29:47 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,11 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("1. Piotr")
-        System.out.println("2. Anna")
-        System.out.println("3. Jan")
-        System.out.println("4. Leszek")
+        System.out.println("1. Piotr");
+        System.out.println("2. Anna");
+        System.out.println("3. Jan");
+        System.out.println("4. Leszek");
         }
     }
-}
+
