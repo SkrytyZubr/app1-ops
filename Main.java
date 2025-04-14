9f91c262ee90458620df8ca6174627a30809cad6
SkrytyZubr <66427481+SkrytyZubr@users.noreply.github.com>
2025-04-14 15:35:29 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,12 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1. Piotr");
-        System.out.println("2. Anna");
-        System.out.println("3. Jan");
-        System.out.println("4. Leszek");
+        List<String> names = List.of("Piotr", "Anna", "Jan", "Leszek");
+        int i = 1;
+        for (String name : names) {
+            System.out.println(i + " " + name);
+            i++;
         }
     }
+}
 
